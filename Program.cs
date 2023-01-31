14ed68a93603170dc212906642ed0735d6fea826
Tope-stack <yusufftope70@gmail.com>
2023-01-31 19:54:40 +0100

    Second Commit

--- Program.cs
+++ Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=TvShows}/{action=Index}/{id?}");
 
 app.Run();
